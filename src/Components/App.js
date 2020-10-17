969103efbb1f5eb9f1fd2d686be9cd2cffb79acc
Kathy Nguyen <kathyn262@gmail.com>
2020-10-16 18:16:25 -0700

    added screen toggle functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeScreen } from '../Actions/screens';
+import Instructions from './Instructions';
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
+  let currentScreen = useSelector(st => st.screen.name);
   // const change = dispatch({screen: 'instructions', type: 'CHANGE_SCREEN'});
 
+  console.log('changing screen to', currentScreen);
+  // const currentScreen = useState('');
+
   function change(name) {
-    dispatch({screen: name, type: 'CHANGE_SCREEN'});
-    console.log('change');
+    dispatch(changeScreen(name));
   }
-  return (
-    <div>
-      <button type="button" className="btn btn-primary">Start</button>
-      <button type="button" className="btn btn-primary" onClick={change('instructions')}>Instructions</button>
-    </div>
-  );
+
+
+
+  if (currentScreen === 'start' || !currentScreen) {
+    console.log('rerender start');
+    return (
+      <div>
+        <button type="button" className="btn btn-primary" onClick={evt => change('start')}>Start</button>
+        <button type="button" className="btn btn-primary" onClick={evt => change('instructions')}>Instructions</button>
+      </div>
+    );
+  } else if (currentScreen === 'instructions') {
+    console.log('rerender at instructions');
+    return (
+      <Instructions />
+    );
+  }
+
+  // return (
+  //   <div>
+  //   {console.log('rendering')}
+  //    <button type="button" className="btn btn-primary" onClick={evt => change('start')}>Start</button>
+  //      <button type="button" className="btn btn-primary" onClick={evt => change('instructions')}>Instructions</button>
+  //   {currentScreen ? 'ok' : 'no'}
+  //    </div>
+  // );
 }
 
 export default App;
--- src/Reducers/screens.js
+++ src/Reducers/screens.js
@@ -3,7 +3,7 @@ import { CHANGE_SCREEN } from '../Actions/types';
 export default function rootReducer(state = {}, action) {
   switch (action.type) {
     case CHANGE_SCREEN:
-      return { ...state, screen: action.screen };
+      return { ...state, name: action.screen };
 
     default:
       return state;
