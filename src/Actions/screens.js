a9a1f09065f2acf6493703463ccb60b18081ff4a
Kathy Nguyen <kathyn262@gmail.com>
2020-10-16 18:15:50 -0700

    added screen name

--- src/Actions/screens.js
+++ src/Actions/screens.js
@@ -3,7 +3,8 @@ import { CHANGE_SCREEN } from './types';
 export function changeScreen(screen) {
   return function (dispatch) {
     return dispatch({
-      type: CHANGE_SCREEN
+      type: CHANGE_SCREEN, 
+      screen
     });
   };
 }
