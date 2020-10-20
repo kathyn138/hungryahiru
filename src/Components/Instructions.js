159d8f58bdcf4836347b5044d9fac15f31f097ed
Kathy Nguyen <kathyn262@gmail.com>
2020-10-19 17:50:34 -0700

    added instructions

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from "react-redux";
 import { changeScreen } from '../Actions/screens';
+import './Instructions.css';
 
 function Instructions() {
   const dispatch = useDispatch();
@@ -10,9 +11,22 @@ function Instructions() {
   }
 
   return (
-    <div>
-      "instructions"
-      <button type="button" className="btn btn-primary" onClick={evt => handleScreenChange('landing')}>Back</button>
+    <div className="container-fluid instructions">
+      <div className="row">
+        <div className="col back-to-main">
+          <h3 className="back-main-menu"
+            onClick={evt => handleScreenChange('landing')}>BACK TO MAIN MENU</h3>
+        </div>
+      </div>
+      <div className="row text-center flex-grow-1">
+        <div className="col instructions-list">
+          <ol>
+            <li>SPACE BAR TO SHOOT FORKS</li>
+            <li>UP ARROW TO JUMP</li>
+          </ol>
+        </div>
+
+      </div>
     </div>
   );
 }
