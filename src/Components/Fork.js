e80f2a5e6d3426fa8ae4be36e240e65ae1bbed51
Kathy Nguyen <kathyn262@gmail.com>
2020-10-20 17:38:35 -0700

    set up fork

---
+++ src/Components/Fork.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useDispatch } from "react-redux";
+import './Fork.css';
+
+function Fork() {
+  const dispatch = useDispatch();
+
+
+  return (
+    <div className="fork">
+        FORK
+    </div>
+  );
+}
+
+export default Fork;
