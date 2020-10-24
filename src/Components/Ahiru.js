c3127b857eff2f1ea7dab98ad401086a82e2d6e1
Kathy Nguyen <kathyn262@gmail.com>
2020-10-23 17:42:38 -0700

    added ahiru jump functionality

--- src/Components/Ahiru.js
+++ src/Components/Ahiru.js
@@ -3,13 +3,15 @@ import { useDispatch } from "react-redux";
 import { changeScreen } from '../Actions/screens';
 import './Ahiru.css';
 
-function Ahiru() {
+function Ahiru(props) {
   const dispatch = useDispatch();
 
   return (
     <div className="ahiru-container">
-      <img src="https://cdn.discordapp.com/attachments/709285942430531650/768257906876612608/ahiru-gif.gif"
-        className="ahiru-gif" alt="player"></img>
+      {props.jump ? <img src="https://cdn.discordapp.com/attachments/709643259789705317/769354247175012392/ahiru-jump.png"
+        className="ahiru-jump" alt="player jump"></img>
+        : <img src="https://cdn.discordapp.com/attachments/709285942430531650/768257906876612608/ahiru-gif.gif"
+          className="ahiru-walk-gif" alt="player"></img>}
     </div>
   );
 }
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from "react-redux";
 import { changeScreen } from '../Actions/screens';
 import Ahiru from './Ahiru';
@@ -8,16 +8,49 @@ import './Game.css';
 
 function Game() {
   const dispatch = useDispatch();
+  const [jump, setJump] = useState(false);
+  const checkJump = useKeyPress('ArrowUp');
 
-  function handleScreenChange(name) {
-    dispatch(changeScreen(name));
+  function useKeyPress(targetKey) {
+
+    function handlePress({ key }) {
+      if (key === targetKey) {
+        setJump(true);
+      }
+    }
+
+    function handleRelease({ key }) {
+      let jumpTimer;
+      if (key === targetKey) {
+        jumpTimer = setTimeout(
+          () => setJump(false), 900);
+      }
+
+      return () => clearTimeout(jumpTimer);
+    }
+
+    useEffect(() => {
+      window.addEventListener('keydown', handlePress);
+      window.addEventListener('keyup', handleRelease);
+
+      return () => {
+        window.removeEventListener('keydown', handlePress);
+        window.removeEventListener('keyup', handleRelease);
+      };
+    });
+
+    return jump;
   }
+
+  // function handleScreenChange(name) {
+  //   dispatch(changeScreen(name));
+  // }
 
   return (
     <div className="game">
       <Score />
       {/* <Fork /> */}
-      <Ahiru />
+      <Ahiru jump={jump} />
     </div>
   );
 }
