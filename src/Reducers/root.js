5a08f353aeb6b7ca4fb0c2b85de141143f01c625
Kathy Nguyen <kathyn262@gmail.com>
2020-11-04 18:37:27 -0800

    added score reducer

--- src/Reducers/root.js
+++ src/Reducers/root.js
@@ -1,7 +1,8 @@
 import forks from './forks';
 import desserts from './desserts';
 import vegetables from './vegetables';
+import score from './score';
 import screen from './screens';
 import { combineReducers } from 'redux';
 
-export default combineReducers({ forks, desserts, vegetables, screen });
+export default combineReducers({ desserts, forks, score, screen, vegetables });
