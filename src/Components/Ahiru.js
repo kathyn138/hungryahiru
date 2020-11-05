ce9839be31c30b7c942cfbb028c601467246bbc0
Kathy Nguyen <kathyn262@gmail.com>
2020-11-04 18:34:36 -0800

    removed extra imports

--- src/Components/Ahiru.js
+++ src/Components/Ahiru.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { useDispatch } from "react-redux";
-import { changeScreen } from '../Actions/screens';
 import './Ahiru.css';
 
 function Ahiru(props) {
-  const dispatch = useDispatch();
-
   return (
     <div className="ahiru-container">
       {props.jump ? <img src="https://cdn.discordapp.com/attachments/709643259789705317/769354247175012392/ahiru-jump.png"
