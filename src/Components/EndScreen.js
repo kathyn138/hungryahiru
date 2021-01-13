09f11754b5434bbe468d59b0a45f7fc26273cd00
Kathy Nguyen <kathyn262@gmail.com>
2021-01-13 14:57:05 -0800

    added reset game functionality

--- src/Components/EndScreen.js
+++ src/Components/EndScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { changeScreen } from '../Actions/screens';
+import { resetScore } from '../Actions/score';
 import './EndScreen.css';
 
 function EndScreen() {
   const dispatch = useDispatch();
   const currScore = useSelector(st => st.score);
+
+  function handleResetGame() {
+    dispatch(changeScreen('landing'));
+    dispatch(resetScore());
+  }
 
   return (
     <div className="container-fluid end-screen">
@@ -16,7 +23,8 @@ function EndScreen() {
       </div>
       <div className="row text-center flex-grow-1">
         <div className="col end-screen-play-again">
-          <p>PLAY AGAIN?</p>
+          <p onClick={evt => handleResetGame()}>
+            PLAY AGAIN?</p>
         </div>
       </div>
     </div>
