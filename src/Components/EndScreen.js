f4c5f14a59c2853dba662f76260135b2d73b1372
Kathy Nguyen <kathyn262@gmail.com>
2021-01-12 16:17:42 -0800

    set up endscreen

--- src/Components/EndScreen.js
+++ src/Components/EndScreen.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import './EndScreen.css';
 
 function EndScreen() {
+  const dispatch = useDispatch();
+  const currScore = useSelector(st => st.score);
+
   return (
-    <div className="container-fluid">
+    <div className="container-fluid end-screen">
       <div className="row text-center flex-grow-1">
-        <div className="col end-screen">
-          game ended
+        <div className="col end-screen-score">
+          <p>SCORE: {currScore.score} </p>
         </div>
 
+      </div>
+      <div className="row text-center flex-grow-1">
+        <div className="col end-screen-play-again">
+          <p>PLAY AGAIN?</p>
+        </div>
       </div>
     </div>
   );
