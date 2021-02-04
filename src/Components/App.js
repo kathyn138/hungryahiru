4f486ce270adc941d3803d79dd473da51461605a
Kathy Nguyen <kathyn262@gmail.com>
2021-02-03 17:16:42 -0800

    added landing screen component for clarity

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,45 +1,16 @@
 import React from 'react';
-import { useDispatch, useSelector } from "react-redux";
-import { changeScreen } from '../Actions/screens';
-import Instructions from './Instructions';
-import UserContainer from './UserContainer';
+import { useSelector } from "react-redux";
 import EndScreen from './EndScreen';
-import DessertContainer from './DessertContainer';
-import VegetableContainer from './VegetableContainer';
-import CollisionContainer from './CollisionContainer';
-import './App.css';
+import Game from './Game';
+import Instructions from './Instructions';
+import LandingScreen from './LandingScreen';
 
 function App() {
-  const dispatch = useDispatch();
   let currentScreen = useSelector(st => st.screen.name);
 
-  function handleScreenChange(name) {
-    dispatch(changeScreen(name));
-  }
-
-
   if (currentScreen === 'landing') {
-    console.log('rerender start');
     return (
-      <div className="container-fluid landing">
-        <div className="row flex-grow-1">
-          <div className="col title">
-            <h1>HUNGRY AHIRU</h1>
-          </div>
-        </div>
-        <div className="row flex-grow-1">
-          <div className="col landing-options">
-            <ol>
-              <li onClick={evt => handleScreenChange('game')}>
-                START GAME
-                </li>
-              <li onClick={evt => handleScreenChange('instructions')}>
-                INSTRUCTIONS
-                </li>
-            </ol>
-          </div>
-        </div>
-      </div>
+      <LandingScreen />
     );
   } else if (currentScreen === 'instructions') {
     return (
@@ -47,12 +18,7 @@ function App() {
     );
   } else if (currentScreen === 'game') {
     return (
-      <React.Fragment>
-        <CollisionContainer />
-        <DessertContainer />
-        <VegetableContainer />
-        <UserContainer />
-      </React.Fragment>
+      <Game />
     );
   } else {
     return (
---
+++ src/Components/LandingScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useDispatch } from "react-redux";
+import { changeScreen } from '../Actions/screens';
+import './LandingScreen.css';
+
+function LandingScreen() {
+  const dispatch = useDispatch();
+
+  function handleScreenChange(name) {
+    dispatch(changeScreen(name));
+  }
+
+  return (
+    <div className="container-fluid landing">
+      <div className="row flex-grow-1">
+        <div className="col title">
+          <h1>HUNGRY AHIRU</h1>
+        </div>
+      </div>
+      <div className="row flex-grow-1">
+        <div className="col landing-options">
+          <ol>
+            <li onClick={evt => handleScreenChange('game')}>
+              START GAME
+                </li>
+            <li onClick={evt => handleScreenChange('instructions')}>
+              INSTRUCTIONS
+                </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingScreen;
